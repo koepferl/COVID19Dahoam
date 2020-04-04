79e6d13fdb45acbb44df6321ad9fff60b8c6bf02
Christine Koepferl <koepferl@mpia.de>
2020-04-04 21:16:14 +0200

    2020-04-04: Downloaded new data

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ for lkid in LK_ID:
     if True: #if lkid == '09182': # to check only one county - nur fuer einen Landkreis
         
         # load from specific csv file
-        num, day, month, name, LK_ids = load_RKI('data_RKI/RKI_COVID19_Bayern_2020-04-02.csv', lkid)
+        num, day, month, name, LK_ids, state = load_RKI('data_RKI/RKI_COVID19_Bayern_2020-04-04.csv', lkid, state_name='Bavaria')
         
         # specify capacity of intensive care for the individual counties - Beatmungskapazitaet
 
